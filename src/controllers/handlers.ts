53c33d85d4c8eca8787e6d51de6dbc0f074f62f2
LonelyBush <lonely.bush68@gmail.com>
2025-05-11 17:12:33 +0300

    refactor: refactored create post body

--- src/controllers/handlers.ts
+++ src/controllers/handlers.ts
@@ -1,6 +1,7 @@
 import { IncomingMessage, ServerResponse } from 'http';
 import users from '../users.json';
 import { create } from './fileOperations';
+import { newUser } from 'types';
 
 export const getAll = (res: ServerResponse<IncomingMessage>) => {
   try {
@@ -36,31 +37,42 @@ export const createNewUser = async (
   req: IncomingMessage,
   res: ServerResponse<IncomingMessage>,
 ) => {
-  let body = '';
-  req.on('data', (chunk) => {
-    body += chunk.toString();
-  });
-  req.on('end', async () => {
+  try {
+    const body = await createPostBody(req);
+    const { username, age, hobbies } = body;
+    if (!username || !age || !hobbies) {
+      throw new Error('Required fields is missing!');
+    }
+    const newUserBody = {
+      username,
+      age,
+      hobbies,
+    };
+    const newUser = await create(newUserBody);
+    res.writeHead(201, {
+      'Content-Type': 'application/json',
+      method: 'POST',
+    });
+    return res.end(JSON.stringify(newUser));
+  } catch (err) {
+    const error = err as Error;
+    res.writeHead(400, { 'Content-Type': 'application/json' });
+    return res.end(JSON.stringify({ error: error.message }));
+  }
+};
+
+export const createPostBody = (req: IncomingMessage): Promise<newUser> => {
+  return new Promise((resolve, reject) => {
     try {
-      const { username, age, hobbies } = JSON.parse(body);
-      if (!username || !age || !hobbies) {
-        throw new Error('Required fields is missing!');
-      }
-      const newUserBody = {
-        username,
-        age,
-        hobbies,
-      };
-      const newUser = await create(newUserBody);
-      res.writeHead(201, {
-        'Content-Type': 'application/json',
-        method: 'POST',
+      let body = '';
+      req.on('data', (chunk) => {
+        body += chunk.toString();
       });
-      return res.end(JSON.stringify(newUser));
+      req.on('end', async () => {
+        resolve(JSON.parse(body));
+      });
     } catch (err) {
-      const error = err as Error;
-      res.writeHead(400, { 'Content-Type': 'application/json' });
-      return res.end(JSON.stringify({ error: error.message }));
+      reject(err);
     }
   });
 };
